8af606b69c77e26a21e0103a8347b34db44de9f7
souha <souhailabenzahra95@gmail.com>
2025-04-03 19:26:43 -0400

    firstGitProject updates

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,7 +3,7 @@ package day01;
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println(
-                "Hello Cydeo!"
+                "Hello Cydeo! \n I hope everything is doing well"
         );
     }
 }
