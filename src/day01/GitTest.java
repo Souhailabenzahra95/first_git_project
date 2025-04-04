47444013ac2d71390ecd1602a64367df4d5b19cd
souha <souhailabenzahra95@gmail.com>
2025-04-03 20:14:08 -0400

    firstGitProject LastUpdates

--- src/day01/GitTest.java
+++ src/day01/GitTest.java
@@ -4,6 +4,6 @@ import org.w3c.dom.ls.LSOutput;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Git has been tested");
+        System.out.println("Git has been tested and passed");
     }
 }
